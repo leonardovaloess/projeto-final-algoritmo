75803ba810cb8b5bd3e59981b761805a56e95bc3
leonardovaloess <leonardoberlandadevaloes@gmail.com>
2024-06-12 11:39:24 -0300

    requests - edit

--- requests.py
+++ requests.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 caminho = os.path.join('interface', 'estoque.txt')
 
-
 #logica para pegar todos as linhas do arquivo txt e transformar em uma lista
 def readProducts():
     caminho = os.path.join('interface', 'estoque.txt')
@@ -22,9 +21,12 @@ def get_next_id(caminho_estoque):
         linhas = arquivo.readlines()
         if not linhas:
             return 1
-        ultima_linha = linhas[-1]
-        ultimo_id = int(ultima_linha.split(',')[0])
-        return ultimo_id + 1
+        else:
+            ultima_linha = linhas[-1].strip()
+            if not ultima_linha:
+                return 1
+            ultimo_id = int(ultima_linha.split(',')[0])
+            return ultimo_id + 1
 
 # Função para criar o produto com ID único
 def createProduct(name, price):
@@ -35,7 +37,7 @@ def createProduct(name, price):
     
     id_unico = get_next_id(caminho)
     
-    product = f'{id_unico}, {data_formatada},{name},R$ {price}\n'
+    product = f'{id_unico}, {data_formatada}, {name}, R$ {price}\n'
     
     with open(caminho, 'a', encoding='utf-8') as arquivo:
         arquivo.write(product)
@@ -53,8 +55,16 @@ def deleteProduct(id):
 
 
 
+def editProduct(id, new_name, new_price):
+    produtos = readProducts()
+    with open(caminho, 'w', encoding='utf-8') as arquivo:
+        for produto in produtos:
+            parts = produto.split(',')
+            if int(parts[0]) == id:
+                data_atual = datetime.now().strftime('%d/%m/%Y')
+                produto = f'{id}, {data_atual}, {new_name}, R$ {new_price}\n'
+            arquivo.write(produto)
 
-createProduct('airpods', '5000')
 
 produtos = readProducts()
 print(produtos)
