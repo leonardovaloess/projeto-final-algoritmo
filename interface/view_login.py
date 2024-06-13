751736261878885308f5f165dac28c1a6203c964
CaioSeniuk <caio.seniuk@gmail.com>
2024-06-13 16:15:25 -0300

    sistema de login, funcao adicionar produto e deletar produto

--- interface/view_login.py
+++ interface/view_login.py
@@ -4,6 +4,15 @@ from customtkinter import *
 from tkinter import *
 from view_principal import gestor
 
+def login():
+    email_inserido = email.get()
+    senha_inserida = senha.get()
+    if email_inserido == "admin" and senha_inserida == "admin12":
+        gestor()
+    else:
+        msg_erro = customtkinter.CTkLabel(janela_login, text="Credenciais Incorretas!")
+        msg_erro.pack(padx=10, pady=40)
+        
 janela_login = customtkinter.CTk()
 janela_login.title("Gestor de Estoque")
 
@@ -16,19 +25,17 @@ customtkinter.set_default_color_theme("dark-blue")
 
 #Área de Login
 texto = customtkinter.CTkLabel(janela_login, text="Fazer Login")
-texto.pack(padx=10, pady=30)
-email = customtkinter.CTkEntry(janela_login, placeholder_text="Seu e-mail", width=300, height=30)
+texto.pack(padx=10, pady=40)
+email = customtkinter.CTkEntry(janela_login, placeholder_text="Seu e-mail", width=200, height=30)
 email.pack()
-senha = customtkinter.CTkEntry(janela_login, placeholder_text="Sua senha", show="*",  width=300, height=30)
-senha.pack(padx=10, pady=10)
-checkbox = customtkinter.CTkCheckBox(janela_login, text="Lembrar Login", corner_radius=15, checkbox_height=30, checkbox_width=30)
-checkbox.place(x= 130, y= 230)
+senha = customtkinter.CTkEntry(janela_login, placeholder_text="Sua senha", show="*",  width=200, height=30)
+senha.pack(padx=10, pady=20)
 
 #Abre a janela do gestor
-botao_login = customtkinter.CTkButton(janela_login, text="Entrar", command=gestor)
-botao_login.place(x=50,y=180)
-botao_sair = customtkinter.CTkButton(janela_login, text="Sair", command=janela_login.destroy)
-botao_sair.place(x=210,y=180)
+botao_login = customtkinter.CTkButton(janela_login, text="Entrar", command=login, width=90, height=30)
+botao_login.place(x=100,y=220)
+botao_sair = customtkinter.CTkButton(janela_login, text="Sair", command=janela_login.destroy,width=90, height=30)
+botao_sair.place(x=210,y=220)
 
 
 #executa a interface
--- interface/view_principal.py
+++ interface/view_principal.py
@@ -1,60 +1,12 @@
-import customtkinter
+import customtkinter, os
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import *
 from view_adicionar_produto import adicionar_produto
-
-def gestor():
-    def barra_pesquisa():
-        pesquisa = procurar.get()
-        print(pesquisa)
-
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("dark-blue")
-    janela_principal = customtkinter.CTk()
-    janela_principal.title("Gestor de Estoque")
-    janela_principal.iconbitmap("interface/images/icon.ico")
-    janela_principal.geometry("950x650+450+100")
-    janela_principal.resizable(False, False)
+from view_deletar_produto import deletar_produto
 
 
-    # Criar um frame para a barra de pesquisa e botões
-    top_frame = customtkinter.CTkFrame(janela_principal)
-    top_frame.pack(side=tk.TOP, fill=tk.X)
-
-    procurar = customtkinter.CTkEntry(top_frame, placeholder_text="Procurar Produto", width=350, height=30)
-    procurar.place(x=100,y=10)
-
-    botao_pesquisar_produto = customtkinter.CTkButton(top_frame, text="Pesquisar Produto", command=barra_pesquisa)
-    botao_pesquisar_produto.place(x= 480, y = 10)
-
-    botao_sair = customtkinter.CTkButton(top_frame, text="Sair", command=janela_principal.destroy)
-    botao_sair.pack(side=RIGHT,padx=10, pady=10)
-
-    botao_adicionar_produto = customtkinter.CTkButton(top_frame, text="Adicionar Produto", command=adicionar_produto)
-    botao_adicionar_produto.pack(side=RIGHT,padx= 10, pady = 10)
-
-
-    # Criar um frame para a tabela
-    tabela = customtkinter.CTkFrame(janela_principal)
-    tabela.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-    tabela.configure(height=400)
-
-    # Criar uma tabela com scrollbars
-    tree = ttk.Treeview(tabela, columns=("Coluna 1", "Coluna 2", "Coluna 3"), show="headings")
-    tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-    # Definir as colunas da tabela
-    tree.heading("Coluna 1", text="Data de inclusão")
-    tree.heading("Coluna 2", text="Nome do Produto")
-    tree.heading("Coluna 3", text="Valor")
-
-    # Criar um scrollbar vertical
-    scrollbar_y = tk.Scrollbar(tabela, orient=tk.VERTICAL, command=tree.yview)
-    scrollbar_y.pack(side=tk.RIGHT, fill=tk.Y)
-    tree.configure(yscrollcommand=scrollbar_y.set)
-
-    # Função para ler dados do arquivo de texto
+def atualizar_tabela():
     def read_text_file(filename):
         data = []
         with open(filename, 'r') as file:
@@ -63,13 +15,87 @@ def gestor():
                 columns = line.strip().split(',')
                 data.append(columns)
         return data
-
+    tree.delete(*tree.get_children())
     # Ler dados do arquivo de texto e preencher o Treeview
-
-    filename = 'C:\\projetos\projeto-final-algoritmo\interface\estoque.txt'
-
+    filename = os.path.join('interface', 'estoque.txt')
     data = read_text_file(filename)
     for row in data:
         tree.insert('', 'end', values=row)
 
-    janela_principal.mainloop()
+
+def barra_pesquisa():
+    pesquisa = procurar.get()
+    print(pesquisa)
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+janela_principal = customtkinter.CTk()
+janela_principal.title("Gestor de Estoque")
+janela_principal.iconbitmap("interface/images/icon.ico")
+janela_principal.geometry("950x650+450+100")
+janela_principal.resizable(False, False)
+
+
+# Criar um frame para a barra de pesquisa e botões
+top_frame = customtkinter.CTkFrame(janela_principal)
+top_frame.pack(side=tk.TOP, fill=tk.X)
+
+procurar = customtkinter.CTkEntry(top_frame, placeholder_text="Procurar Produto", width=200, height=30)
+procurar.place(x=5,y=10)
+
+botao_pesquisar_produto = customtkinter.CTkButton(top_frame, text="Pesquisar Produto", command=barra_pesquisa)
+botao_pesquisar_produto.place(x= 210, y = 10)
+
+botao_adicionar_produto = customtkinter.CTkButton(top_frame, text="Adicionar Produto", command=adicionar_produto)
+botao_adicionar_produto.place(x=360, y=10)
+
+botao_deletar_produto = customtkinter.CTkButton(top_frame, text="Deletar Produto", command=deletar_produto)
+botao_deletar_produto.place(x=508, y=10)
+
+botao_atualizar_tabela = customtkinter.CTkButton(top_frame, text="Atualizar Tabela", command=atualizar_tabela)
+botao_atualizar_tabela.place(x=655, y=10)
+
+botao_sair = customtkinter.CTkButton(top_frame, text="Sair", command=janela_principal.destroy)
+botao_sair.pack(side=RIGHT,padx=10, pady=10)
+
+
+
+# Criar um frame para a tabela
+tabela = customtkinter.CTkFrame(janela_principal)
+tabela.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+tabela.configure(height=400)
+
+# Criar uma tabela com scrollbars
+tree = ttk.Treeview(tabela, columns=("Coluna 1", "Coluna 2", "Coluna 3", "Coluna 4"), show="headings")
+tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+# Definir as colunas da tabela
+tree.heading("Coluna 1", text="ID")
+tree.heading("Coluna 2", text="Data de inclusão")
+tree.heading("Coluna 3", text="Nome do Produto")
+tree.heading("Coluna 4", text="Valor")
+
+# Criar um scrollbar vertical
+scrollbar_y = tk.Scrollbar(tabela, orient=tk.VERTICAL, command=tree.yview)
+scrollbar_y.pack(side=tk.RIGHT, fill=tk.Y)
+tree.configure(yscrollcommand=scrollbar_y.set)
+
+# Função para ler dados do arquivo de texto
+def read_text_file(filename):
+    data = []
+    with open(filename, 'r') as file:
+        for line in file:
+            # Dividir cada linha em colunas separadas por tabulação
+            columns = line.strip().split(',')
+            data.append(columns)
+    return data
+
+# Ler dados do arquivo de texto e preencher o Treeview
+
+filename = os.path.join('interface', 'estoque.txt')
+
+data = read_text_file(filename)
+for row in data:
+    tree.insert('', 'end', values=row)
+
+janela_principal.mainloop()
