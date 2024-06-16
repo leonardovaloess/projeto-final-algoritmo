d9c6ca27dca7165edf06e7a7918969a44eaebfeb
CaioSeniuk <caio.seniuk@gmail.com>
2024-06-16 01:20:08 -0300

    versão final

--- interface/teste.py
+++ interface/teste.py
@@ -4,6 +4,27 @@ from tkinter import ttk
 from customtkinter import *
 from requests import editProduct, readProducts # type: ignore
 
+def read_text_file(filename):
+    data = []
+    with open(filename, 'r') as file:
+        for line in file:
+            # Dividir cada linha em colunas separadas por tabulação
+            columns = line.strip().split(',')
+            data.append(columns)
+    return data
+
+def procurar_produto():
+    tree.delete(*tree.get_children())
+    produto_pesquisado = nome_produto.get()
+    data = []
+    filename = os.path.join('interface', 'estoque.txt')
+    with open(filename, "r") as arquivo:
+        for linha in arquivo:
+            columns = linha.strip().split(',')
+            data.append(columns)
+        for produto_pesquisado in data:
+            if produto_pesquisado in data:
+                tree.insert('','end',values=produto_pesquisado)
 
 root=tk.Tk()
 tabela = ctk.CTkFrame(root)
@@ -16,7 +37,7 @@ tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 nome_produto = ctk.CTkEntry(root, placeholder_text="Nome", width=150, height=30) 
 nome_produto.pack(padx=10,pady=20)
 
-botao_salvar_edicao = ctk.CTkButton(root, text="Salvar", width=100, command=lambda:pesquisar_produto(tree))
+botao_salvar_edicao = ctk.CTkButton(root, text="Salvar", width=100, command=procurar_produto)
 botao_salvar_edicao.pack(pady=10)
 
 # Definir as colunas da tabela
@@ -29,14 +50,6 @@ tree.heading("Coluna 4", text="Valor")
 
 # C ----------------------------------------------------------------------------------------------------------------------------------------------------------
 # Função para ler dados do arquivo de texto
-def read_text_file(filename):
-    data = []
-    with open(filename, 'r') as file:
-        for line in file:
-            # Dividir cada linha em colunas separadas por tabulação
-            columns = line.strip().split(',')
-            data.append(columns)
-    return data
 
 # C ----------------------------------------------------------------------------------------------------------------------------------------------------------
 # Ler dados do arquivo de texto e preencher o Treeview
@@ -48,14 +61,7 @@ for linha in data:
 
 
 # Looping para armazenar os itens de cada linha separadamente e filtrar o nome
-produto_pesquisado = "Iphone 12"
-nome_pesquisado= []
-with open(filename, 'r') as file:
-    for linha in file:
-        itens = linha.strip().split(',')
-        nome_pesquisado.append(itens[2])
-    for i in nome_pesquisado:
-        if i == produto_pesquisado:
-            print("Encontrado")
-        else:
-            print("Não encontrado")
+            
+           
+
+root.mainloop()
--- interface/view_principal.py
+++ interface/view_principal.py
@@ -1,32 +1,44 @@
-import customtkinter as ctk, os
+import customtkinter as ctk
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import *
+import os
 from view_adicionar_produto import adicionar_produto
 from view_deletar_produto import deletar_produto
 from view_editar_produto import editar_produto
 
+
 def gestor(janela_login):
 
-    # Função para ler todos os dados do arquivo txt e preencher o Treeview
-    def atualizar_tabela(tree):
-        def read_text_file(filename):
-            data = []
-            with open(filename, 'r') as file:
-                for line in file:
-                    # Dividir cada linha em colunas separadas por tabulação
-                    columns = line.strip().split(',')
-                    data.append(columns)
-            return data
+    def read_text_file(filename):
+        data = []
+        with open(filename, 'r') as file:
+            for line in file:
+                columns = line.strip().split(',')
+                data.append(columns)
+        return data
+    
+    def atualizar_tabela(data):
         tree.delete(*tree.get_children())
-
-        # Ler dados do arquivo de texto através da função e preencher o Treeview com o for
-        filename = os.path.join('interface', 'estoque.txt')
-        data = read_text_file(filename)
         for row in data:
             tree.insert('', 'end', values=row)
 
-    # Define dimensões, icone e onde a janela irá "nascer"
+    def buscar_produto():
+        palavra_chave = procurar.get().strip().lower()
+        if palavra_chave == "":
+            return
+        # Limpar o Treeview
+        # Ler dados do arquivo de texto
+        filename = os.path.join('interface', 'estoque.txt')
+        data = read_text_file(filename)
+        # Preencher o Treeview apenas com os resultados da pesquisa
+        for row in data:
+            if palavra_chave in row[2].lower():  # Comparar com o nome do produto (terceira coluna)
+                tree.delete(*tree.get_children())
+                tree.insert('', 'end', values=row)
+            else:
+                procurar_texto.configure(text="Não foi possível encontrar este produto")
+
+
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("dark-blue")
     janela_principal = ctk.CTk()
@@ -35,80 +47,54 @@ def gestor(janela_login):
     janela_principal.geometry("950x650+490+150")
     janela_principal.resizable(False, False)
 
-    # Lista e função para armazenar as janelas para poder fechar todas de uma vez só
     janelas = [janela_login, janela_principal]
     def fechar_janelas(janelas):
         for janela in janelas:
             janela.destroy()
-
-    # Criar um frame para a barra de pesquisa e botão para pesquisar produto ficarem em cima
-    top_frame = ctk.CTkFrame(janela_principal,fg_color="#1a1a1a")
+    
+    top_frame = ctk.CTkFrame(janela_principal, fg_color="#1a1a1a")
     top_frame.pack(side=tk.TOP, fill=tk.BOTH)
 
-    # Criando o Entry para procurar produto
-    procurar = ctk.CTkEntry(top_frame, placeholder_text="Procurar Produto", width=200)
-    procurar.grid(row=0, column=0, padx=5, pady=10, columnspan=3)
+    procurar = ctk.CTkEntry(top_frame, placeholder_text="Procurar Produto", width=200,height=30)
+    procurar.grid(row=0, column=0, padx=5, pady=30, columnspan=3)
 
-    # Botão para pesquisar produto
-    botao_pesquisar_produto = ctk.CTkButton(top_frame, text="Pesquisar Produto", width=20)
+    procurar_texto = ctk.CTkLabel(top_frame, text="")
+    procurar_texto.place(x=10,y=-2)
+
+    botao_pesquisar_produto = ctk.CTkButton(top_frame, text="Pesquisar Produto", width=20,height=35,command=buscar_produto)
     botao_pesquisar_produto.grid(row=0, column=3, padx=5, pady=10)
 
-    # Botão para editar produto
-    botao_editar_produto = ctk.CTkButton(top_frame, text="Editar Produto", width=100, command=lambda:editar_produto(janela_principal,tree))
+    botao_editar_produto = ctk.CTkButton(top_frame, text="Editar Produto", width=100,height=35, command=lambda:editar_produto(janela_principal, tree))
     botao_editar_produto.grid(row=0, column=4, padx=5, pady=10)
 
-
-    # Botão para adicionar produto
-    botao_adicionar_produto = ctk.CTkButton(top_frame, text="Adicionar Produto", command=lambda: adicionar_produto(janela_principal), width=20)
+    botao_adicionar_produto = ctk.CTkButton(top_frame, text="Adicionar Produto", command=lambda: adicionar_produto(janela_principal), width=20, height=35)
     botao_adicionar_produto.grid(row=0, column=5, padx=5, pady=10)
 
-    # Botão para deletar produto
-    botao_deletar_produto = ctk.CTkButton(top_frame, text="Deletar Produto", command=lambda:deletar_produto(janela_principal), width=20)
+    botao_deletar_produto = ctk.CTkButton(top_frame, text="Deletar Produto", command=lambda:deletar_produto(janela_principal), width=20, height=35)
     botao_deletar_produto.grid(row=0, column=6, padx=5, pady=10)
 
-    # Botão para atualizar tabela
-    botao_atualizar_tabela = ctk.CTkButton(top_frame, text="Atualizar Tabela", command=lambda: atualizar_tabela(tree), width=20)
+    botao_atualizar_tabela = ctk.CTkButton(top_frame, text="Atualizar Tabela", command=lambda: atualizar_tabela(read_text_file(filename)), width=20, height=35)
     botao_atualizar_tabela.grid(row=0, column=7, padx=5, pady=10)
 
-    # Botão para sair
-    botao_sair = ctk.CTkButton(top_frame, text="Sair", command=lambda:fechar_janelas(janelas), width=120)
+    botao_sair = ctk.CTkButton(top_frame, text="Sair", command=lambda:fechar_janelas(janelas), width=120, height=35)
     botao_sair.grid(row=0, column=8, padx=5, pady=10)
 
-
-
-    # Criar um frame para a tabela
     tabela = ctk.CTkFrame(janela_principal)
-    tabela.pack(side=tk.LEFT, fill=BOTH, expand=True)
+    tabela.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-    # Criar uma tabela com scrollbars
     tree = ttk.Treeview(tabela, columns=("Coluna 1", "Coluna 2", "Coluna 3", "Coluna 4"), show="headings")
     tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-    # Definir as colunas da tabela
     tree.heading("Coluna 1", text="ID")
     tree.heading("Coluna 2", text="Data de inclusão")
     tree.heading("Coluna 3", text="Nome do Produto")
     tree.heading("Coluna 4", text="Valor")
 
-    # Criar um scrollbar vertical
     scrollbar_y = tk.Scrollbar(tabela, orient=tk.VERTICAL, command=tree.yview)
     scrollbar_y.pack(side=tk.RIGHT, fill=tk.Y)
     tree.configure(yscrollcommand=scrollbar_y.set)
 
-    # Função para ler dados do arquivo de texto
-    def read_text_file(filename):
-        data = []
-        with open(filename, 'r') as file:
-            for line in file:
-                # Dividir cada linha em colunas separadas por tabulação
-                columns = line.strip().split(',')
-                data.append(columns)
-        return data
-
-    # Ler dados do arquivo de texto e preencher o Treeview
-
     filename = os.path.join('interface', 'estoque.txt')
-
     data = read_text_file(filename)
     for row in data:
         tree.insert('', 'end', values=row)
